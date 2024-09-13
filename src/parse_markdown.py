3cc3f9f5e0371e46779cb8f10ee504482fbc4494
David Kagan <david.kagan07@gmail.com>
2024-09-13 12:59:07 -0400

    Add functionality to parse images and links in MD with regex
    3.4

--- src/parse_markdown.py
+++ src/parse_markdown.py
@@ -1,3 +1,4 @@
+import re
 from textnode import TextNode
 
 
@@ -41,3 +42,20 @@ def split_nodes_with_delimiter(old_nodes, delimiter, text_type):
         result.extend(new_nodes)
 
     return result
+
+
+def extract_markdown_images(text):
+    """extract_markdown_images takes raw markdown for images and returns
+    a list of tuples following the format: [(alt-text, link), ...]"""
+
+    return re.findall(r"!\[(.*?)\]\((.*?)\)", text)
+
+
+def extract_markdown_links(text):
+    """extract_markdown_links takes raw markdown for links and returns a list of
+    tuples following the format: [(anchor_text, url), ...]"""
+
+    # the difference here between the images and the links is that the links
+    # regex makes sure that the alt text in the [] isn't preceeded by a "!" like
+    # the images syntax
+    return re.findall(r"(?<!!)\[(.*?)\]\((.*?)\)", text)
--- src/test_parse_markdown.py
+++ src/test_parse_markdown.py
@@ -1,6 +1,10 @@
 import unittest
 
-from parse_markdown import split_nodes_with_delimiter
+from parse_markdown import (
+    extract_markdown_images,
+    extract_markdown_links,
+    split_nodes_with_delimiter,
+)
 from textnode import TextNode
 
 
@@ -68,3 +72,63 @@ class TestParseMarkdown(unittest.TestCase):
 
         with self.assertRaises(ValueError):
             split_nodes_with_delimiter([node], "*", "italic")
+
+
+class TestExtractMDImages(unittest.TestCase):
+    def test_extract_md_images(self):
+        img_text = (
+            "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) image"
+        )
+
+        result = extract_markdown_images(img_text)
+        self.assertListEqual([("rick roll", "https://i.imgur.com/aKaOqIh.gif")], result)
+
+    def test_extract_mult_images(self):
+        img_text = "This is text with multiple images, including ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
+
+        result = extract_markdown_images(img_text)
+        self.assertListEqual(
+            [
+                ("rick roll", "https://i.imgur.com/aKaOqIh.gif"),
+                ("obi wan", "https://i.imgur.com/fJRm4Vk.jpeg"),
+            ],
+            result,
+        )
+
+    def test_extract_img_no_alt(self):
+        img_text = (
+            "This is an image with no alt text ![](https://i.imgur.com/aKaOqIh.gif)"
+        )
+
+        result = extract_markdown_images(img_text)
+
+        self.assertListEqual([("", "https://i.imgur.com/aKaOqIh.gif")], result)
+
+
+class TestExtractMDLinks(unittest.TestCase):
+    def test_extract_links(self):
+        link_text = "This is text with a link [to boot dev](https://www.boot.dev)"
+
+        result = extract_markdown_links(link_text)
+
+        self.assertListEqual([("to boot dev", "https://www.boot.dev")], result)
+
+    def test_multi_image_extract_links(self):
+        link_text = "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
+
+        result = extract_markdown_links(link_text)
+
+        self.assertListEqual(
+            [
+                ("to boot dev", "https://www.boot.dev"),
+                ("to youtube", "https://www.youtube.com/@bootdotdev"),
+            ],
+            result,
+        )
+
+    def test_image_extract_no_anchor_text(self):
+        link_text = "This is text with a link [](https://www.boot.dev)"
+
+        result = extract_markdown_links(link_text)
+
+        self.assertListEqual([("", "https://www.boot.dev")], result)
