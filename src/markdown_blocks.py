acceb7cc0752b498a8eab91ca579a8e1d1879c02
David Kagan <david.kagan07@gmail.com>
2024-10-11 18:19:38 -0400

    Implemented markdown block type
    4.2

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -1,3 +1,16 @@
+"""markdown_blocks.py holds the logic to break down blocks and tell us which
+type they are"""
+
+import re
+
+BLOCK_HEADING = "heading"
+BLOCK_PARAGRAPH = "paragraph"
+BLOCK_CODE = "code"
+BLOCK_QUOTE = "quote"
+BLOCK_UNORDERED_LIST = "unordered_list"
+BLOCK_ORDERED_LIST = "ordered_list"
+
+
 def markdown_to_blocks(document):
     """markdown_to_blocks takes a document with blocks of MD and returns a list
     of strings, where each item in the list is a block of text"""
@@ -16,3 +29,29 @@ def markdown_to_blocks(document):
         res.append(block)
 
     return res
+
+
+def md_block_to_block_type(block):
+    """takes a block of MD and returns a string representing what type of block it is"""
+
+    if len(re.findall(r"^#{1,6} ", block)) > 0:
+        return BLOCK_HEADING
+
+    if len(re.findall(r"(?s)^```.*```$", block)) > 0:
+        return BLOCK_CODE
+
+    if block[0] == ">":
+        return BLOCK_QUOTE
+
+    split_blocks = block.split("\n")
+
+    for i in range(len(split_blocks)):
+        split_blocks[i] = split_blocks[i].strip()
+
+    if len(split_blocks) > 1:
+        if split_blocks[0][0] == "*" or split_blocks[0][0] == "-":
+            return BLOCK_UNORDERED_LIST
+        if split_blocks[0][0] == "1":
+            return BLOCK_ORDERED_LIST
+
+    return BLOCK_PARAGRAPH
--- src/test_markdown_blocks.py
+++ src/test_markdown_blocks.py
@@ -1,6 +1,14 @@
 import unittest
 
-from markdown_blocks import markdown_to_blocks
+from markdown_blocks import (
+    BLOCK_HEADING,
+    BLOCK_ORDERED_LIST,
+    BLOCK_CODE,
+    BLOCK_PARAGRAPH,
+    BLOCK_UNORDERED_LIST,
+    markdown_to_blocks,
+    md_block_to_block_type,
+)
 
 
 class TestMDBlockToText(unittest.TestCase):
@@ -41,3 +49,40 @@ class TestMDBlockToText(unittest.TestCase):
         res = ["# Heading", "Paragraph 1", "Paragraph 2"]
 
         self.assertListEqual(res, markdown_to_blocks(text))
+
+
+class TestMDBlockToBlockType(unittest.TestCase):
+    def test_multiple_block_types(self):
+        text_block = """### Heading
+
+        Paragraph
+
+
+        ```
+        This is a code block
+        ```
+
+        * list 1
+        * list 2
+        * list 3
+
+
+        1. ordered 1
+        2. ordered 2
+        3. ordered 3
+        
+        """
+
+        res = [
+            BLOCK_HEADING,
+            BLOCK_PARAGRAPH,
+            BLOCK_CODE,
+            BLOCK_UNORDERED_LIST,
+            BLOCK_ORDERED_LIST,
+        ]
+
+        blocks = markdown_to_blocks(text_block)
+
+        for idx, block in enumerate(blocks):
+            r = md_block_to_block_type(block)
+            self.assertEqual(res[idx], r)
